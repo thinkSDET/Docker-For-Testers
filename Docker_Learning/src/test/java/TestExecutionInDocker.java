96b18cc1331df8cac01f7357a0bca3b3fc8209a8
Qa-Automation30 <qautomation32@gmail.com>
2025-04-12 18:04:15 +0530

    First setup of docker execution

--- Docker_Learning/src/test/java/TestExecutionInDocker.java
+++ Docker_Learning/src/test/java/TestExecutionInDocker.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URI;
-
+// https://www.youtube.com/watch?v=daILiXCLom8&list=PL9ok7C7Yn9A9jiuK3iGfC6lFCQtxxD9sX&index=7
 public class TestExecutionInDocker {
 
     /**
@@ -14,6 +14,20 @@ public class TestExecutionInDocker {
      */
     @Test
     public void testOne(){
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName(Browser.CHROME.browserName());
+        try {
+            WebDriver driver = new RemoteWebDriver(URI.create("http://localhost:8888").toURL(), capabilities);
+            driver.get("https://demoqa.com/");
+            System.out.println("This is my page title ->"+driver.getTitle());
+            Thread.sleep(2000);
+            driver.quit();
+        } catch (MalformedURLException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void testTwo(){
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setBrowserName(Browser.CHROME.browserName());
         try {
