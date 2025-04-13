13268fd3d81ce1224b941cc44c8c3d2cfbe9aba4
Qa-Automation30 <qautomation32@gmail.com>
2025-04-13 19:50:40 +0530

    Added pages

---
+++ Docker_Learning/src/test/java/com/docker/common/BasePage.java
@@ -0,0 +1,24 @@
+package com.docker.common;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public abstract class BasePage {
+    protected final WebDriver driver;
+    protected final WebDriverWait wait;
+    public BasePage(WebDriver driver){
+        this.driver =  driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        PageFactory.initElements(driver,this);
+    }
+
+    /**
+     * "Before you interact with any element or method on the page,
+     * first confirm that you're actually on that page, and it has loaded successfully."
+     * @return
+     */
+    public abstract boolean isAtExpectedPage();
+}
